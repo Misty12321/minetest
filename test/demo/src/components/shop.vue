67957add4c9eccad53a5b01c68ea0a7c5105dcf6
asdasd12938 <2380279488@qq.com>
2023-06-09 18:32:32 +0800

    11

---
+++ test/demo/src/components/shop.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="shop-item">
+        <img :src="info.images" alt="">
+        <p>{{ info.title }}</p>
+        <p><b>￥{{ info.price }}</b><i></i></p>
+    </div>
+
+</template>
+<script>a
+export default{
+    props:['info']
+}</script>
+<style>
+.shop-item{
+    width: 200px;
+    height: 400px;
+    padding: 10px;
+    box-sizing: border-box;
+}
+.shop-item img{
+    width: 180px;
+    height: 240px;
+}
+.shop-item p{
+    font-size: 14px;
+    color: aqua;
+}
+.shop-item b{
+    color: red;
+}
+.shop-item i{
+    cursor: pointer;
+}
+</style>
